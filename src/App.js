387bdc0ef5cae960c5bfa836b8401bf1ce35ab85
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-01 00:04:00 -0800

    waterfrontDropdown func created

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
       <h1>
         {/* dynamically update the city name here using state */}
-        Welcome to beautiful Portland!
+        Welcome to beautiful {cityName}!
       </h1>
       <div className='bottom'>
         {/* here, the CityNameInput component takes in the setCityName state handler function */}
@@ -34,6 +34,7 @@ function App() {
           
           In each Dropdown, pass as props the state handler functions defined above, so that these child components can manipulate parent state 
           */}
+          <WaterfrontDropdown setWaterfrontId={setWaterfrontId} />
         </section>
         {/* here, the SloganForm component takes in the setSlogans state handler function and the slogans array that live in state */}
         {/* here, the SloganList component takes the array of slogans that lives in state */}
