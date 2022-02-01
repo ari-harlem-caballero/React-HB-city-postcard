fe919f7361babe72708f93b7f53c5b9b891a9513
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-01 00:14:40 -0800

    added img func

--- src/App.js
+++ src/App.js
@@ -22,6 +22,10 @@ function App() {
   return (
     <div className="App">
       {/* here, the City component takes in skylineId, waterfrontId, castleId as props. It'll use those ids to render pictures correctly. */}
+      <City
+        waterfrontId={waterfrontId}
+        skylineId={skylineId}
+        castleId={castleId} />
       <h1>
         {/* dynamically update the city name here using state */}
         Welcome to beautiful {cityName}!
